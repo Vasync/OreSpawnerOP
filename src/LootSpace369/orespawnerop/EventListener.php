f2bac208f709c989af77383aa53a9ab423c884c0
LSpace <tbao12901@gmail.com>
2024-08-02 22:44:33 +0700

    Update EventListener.php

--- src/LootSpace369/orespawnerop/EventListener.php
+++ src/LootSpace369/orespawnerop/EventListener.php
@@ -14,16 +14,16 @@ class EventListener implements Listener {
   public function onPlace(BlockPlaceEvent $ev) {
       $item = $ev->getItem();
     
-      if ($item->hasTag('osp')) {
+      if ($item->getNamedTag()->hasTag('osp')) {
           $pos = $ev->getTransaction()->getBlocks();
-          Main::setData($pos[0] .','. $pos[1] + 1 .','. $pos[2] .','. $ev->getPlayer()->getWorld()->getFolderName() .','. $item->getString('osp'));
+          Main::setData($pos[0] .','. ($pos[1] + 1) .','. $pos[2] .','. $ev->getPlayer()->getWorld()->getFolderName() .','. $item->getNamedTag()->getString('osp'));
       }
   }
   
   public function onBreak(BlockBreakEvent $ev) {
       $block = $ev->getBlock();
       $pos = $block->getPosition();
-      $ktra = $pos[0] .','. $pos[1] + 1 .','. $pos[2] .','. $pos->getWorld()->getFolderName();
+      $ktra = $pos[0] .','. ($pos[1] + 1) .','. $pos[2] .','. $pos->getWorld()->getFolderName();
       if (Main::existsData($ktra)) {
           Main::removeData($ktra);
           foreach ($ev->getDrops() as $drop) {
