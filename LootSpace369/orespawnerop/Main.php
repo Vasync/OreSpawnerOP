1204eb0ef08771878af9c26f94fc1ba79af871f0
LSpace <tbao12901@gmail.com>
2024-08-02 21:38:54 +0700

    Update Main.php

--- LootSpace369/orespawnerop/Main.php
+++ LootSpace369/orespawnerop/Main.php
@@ -35,7 +35,7 @@ class Main extends PluginBase {
   public static function exists(string $data): bool {
       foreach (self::getData() as $osp) {
           $exosp = explode($osp);
-          if ($data == $exosp[0] .','. $exosp[1] .','. $exosp[2] .',' $exosp[3]) {
+          if ($data == $exosp[0] .','. $exosp[1] + 1 .','. $exosp[2] .',' $exosp[3]) {
             return true;
           }
       }
