d7f4e78b4bbb1f251b97b4229c23e72845ffda55
LSpace <tbao12901@gmail.com>
2024-08-03 00:37:18 +0700

    Update Main.php

--- src/LootSpace369/orespawnerop/Main.php
+++ src/LootSpace369/orespawnerop/Main.php
@@ -64,8 +64,9 @@ class Main extends PluginBase {
               if (!isset($args[0]) or !in_array($args[0], $type)) { $player->sendMessage(TF::RED .'Please, input ore spawner type!'); return false; }
 
               if (in_array($args[0], $type)) {
-                  if (!isset($args[1])) { $this->giveOSP($player, $args[0]); return true; }
-                  $this->giveOSP($player, $args[0], (string)$args[1]);
+                  $arg[2] ?? $arg[2] = 1;
+                  if (!isset($args[1])) { $this->giveOSP($player, $args[0], $args[2]); return true; }
+                  $this->giveOSP($player, $args[0], $args[2], (string)$args[1]);
                   
                   return true;
               }
@@ -74,9 +75,9 @@ class Main extends PluginBase {
       return false;
   }
 
-  public function giveOSP(Player $player, string $type, $name = 'ore spawner') {
+  public function giveOSP(Player $player, string $type, int $count = 1, $name = 'ore spawner') {
 
-      $item = StringToItemParser::getInstance()->parse($type.'_block')->setCustomName($name);
+      $item = StringToItemParser::getInstance()->parse($type.'_block')->setCustomName($name)->setCount($count);
       $item->getNamedTag()->setString('osp', $type.'_ore');
 
       $player->getInventory()->addItem($item);
